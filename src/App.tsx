11a58eecfb0157e1bd316b2a398170e3f79c0464
h-0y28 <s24015@gsm.hs.kr>
2024-12-12 10:44:04 +0900

    feat: add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import NotFound from "./pages/NotFound";
+import Navbar from "./components/layout/Navbar";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/diary" element={<Diary />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <Navbar />
     </Router>
   );
 };
