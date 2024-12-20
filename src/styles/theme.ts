f325749af9b3d58157dadcbf52e6736fb1f24c99
h-0y28 <s24015@gsm.hs.kr>
2024-12-20 15:50:15 +0900

    setting: theme color and font

---
+++ src/styles/theme.ts
@@ -0,0 +1,16 @@
+export const theme = {
+  colors: {
+    primaryBackground: "#FFF7E8",
+    primaryBlack: "#2B2B2B",
+    primaryGray: "#B0BEC5",
+    primaryWhite: "#FFFFF3",
+
+    primaryRed: "#D67D6D",
+    primaryYellow: "#FFEECD",
+    primaryNavy: "#34495E",
+    primaryBrown: "#A67B5B",
+  },
+  fonts: {
+    sans: "'Pretendard', system-ui",
+  },
+};
