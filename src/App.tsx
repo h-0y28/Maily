d908371dbc537815e96d687d081452d79297017e
h-0y28 <s24015@gsm.hs.kr>
2024-12-20 14:51:57 +0900

    feat: add layout

--- src/App.tsx
+++ src/App.tsx
@@ -9,27 +9,31 @@ import DiaryYear from "./pages/DiaryYear";
 import Edit from "./pages/Edit";
 import Pick from "./pages/Pick";
 import Profile from "./pages/Profile";
+import Layout from "./layout/Layout";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        {/* 메인 페이지 */}
-        <Route path="/" element={<Home />} />
-        <Route path="/intro" element={<Intro />} />
+        {/* 공통 레이아웃 적용 페이지 */}
+        <Route path="/" element={<Layout />}>
+          {/* 메인 페이지 */}
+          <Route index element={<Home />} />
+          <Route path="intro" element={<Intro />} />
 
-        {/* 다이어리 관련 페이지 */}
-        <Route path="/write" element={<Write />} />
-        <Route path="/diaryMonth" element={<DiaryMonth />} />
-        <Route path="/diaryYear" element={<DiaryYear />} />
-        <Route path="/detail/:id" element={<Detail />} />
-        <Route path="/edit/:id" element={<Edit />} />
+          {/* 다이어리 관련 페이지 */}
+          <Route path="write" element={<Write />} />
+          <Route path="diaryMonth" element={<DiaryMonth />} />
+          <Route path="diaryYear" element={<DiaryYear />} />
+          <Route path="detail/:id" element={<Detail />} />
+          <Route path="edit/:id" element={<Edit />} />
 
-        {/* 기타 페이지 */}
-        <Route path="/pick" element={<Pick />} />
-        <Route path="/profile" element={<Profile />} />
+          {/* 기타 페이지 */}
+          <Route path="pick" element={<Pick />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
 
-        {/* 404 페이지 */}
+        {/* 404 페이지 (공통 레이아웃 제외) */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
