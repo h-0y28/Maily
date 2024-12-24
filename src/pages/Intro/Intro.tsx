0e3f32eb0ed002210813c2776e804c4d1d2ed288
h-0y28 <s24015@gsm.hs.kr>
2024-12-24 10:14:18 +0900

    feat: add explain

--- src/pages/Intro/Intro.tsx
+++ src/pages/Intro/Intro.tsx
@@ -1,9 +1,11 @@
+import Explain from "../../components/Intro/Explain";
 import Welcome from "../../components/Intro/Welcome";
 
 export default function Intro() {
   return (
     <>
       <Welcome />
+      <Explain />
     </>
   );
 }
