0f01260632e2071c62197dc9681e86ac31b48d39
h-0y28 <s24015@gsm.hs.kr>
2024-12-26 18:21:55 +0900

    chore: 파일 이동동

--- src/components/auth/LoginButton/LoginButton.tsx
+++ src/components/auth/LoginButton/LoginButton.tsx
@@ -1,14 +1,9 @@
-import { useNavigate } from "react-router-dom";
-import { handleLogin } from "../utils/authFunctions";
+import useAuth from "../utils/authFunctions";
 import * as S from "./LoginButton.styles";
 
 const LoginButton = () => {
-  const navigate = useNavigate();
-  return (
-    <S.ButtonContainer onClick={() => handleLogin(navigate)}>
-      Google로 로그인
-    </S.ButtonContainer>
-  );
+  const { login } = useAuth();
+  return <S.ButtonContainer onClick={login}>Google로 로그인</S.ButtonContainer>;
 };
 
 export default LoginButton;
