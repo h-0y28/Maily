f65e9ca77d3bf35d7bac7fff6b607933164dde6b
h-0y28 <s24015@gsm.hs.kr>
2024-12-26 17:12:29 +0900

    feat: 로그인 후 home으로 이동동

--- src/components/auth/LoginButton/LoginButton.tsx
+++ src/components/auth/LoginButton/LoginButton.tsx
@@ -1,9 +1,13 @@
+import { useNavigate } from "react-router-dom";
 import { handleLogin } from "../utils/authFunctions";
 import * as S from "./LoginButton.styles";
 
 const LoginButton = () => {
+  const navigate = useNavigate();
   return (
-    <S.ButtonContainer onClick={handleLogin}>Google로 로그인</S.ButtonContainer>
+    <S.ButtonContainer onClick={() => handleLogin(navigate)}>
+      Google로 로그인
+    </S.ButtonContainer>
   );
 };
 
--- src/components/auth/utils/authFunctions.ts
+++ src/components/auth/utils/authFunctions.ts
@@ -1,11 +1,13 @@
 import { signInWithPopup } from "firebase/auth";
 import { auth, provider } from "./firebase";
+import { useNavigate } from "react-router-dom";
 
-export const handleLogin = async () => {
+export const handleLogin = async (navigate: ReturnType<typeof useNavigate>) => {
   try {
     const result = await signInWithPopup(auth, provider);
     const user = result.user;
     console.log("User logged in: ", user);
+    navigate("/home");
   } catch (error) {
     console.error("Login error: ", error);
   }
