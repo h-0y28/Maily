c500c2adf94649f278495769f330bb248e9f353f
h-0y28 <s24015@gsm.hs.kr>
2024-12-26 21:09:31 +0900

    add: calendar component

---
+++ src/pages/CalendarPage/CalendarPage.tsx
@@ -0,0 +1,12 @@
+import "react-calendar/dist/Calendar.css";
+import CalendarComponent from "../../components/Calendar";
+
+const CalendarPage = () => {
+  return (
+    <div>
+      <CalendarComponent />
+    </div>
+  );
+};
+
+export default CalendarPage;
