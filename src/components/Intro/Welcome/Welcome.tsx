70426c0536ada150e7400c67169490c5b8df8334
h-0y28 <s24015@gsm.hs.kr>
2024-12-26 20:23:44 +0900

    feat: 로그인 상태에 따른 화면

--- src/components/Intro/Welcome/Welcome.tsx
+++ src/components/Intro/Welcome/Welcome.tsx
@@ -1,16 +1,42 @@
-import LoginButton from "../../auth/LoginButton/LoginButton";
+import { useEffect, useState } from "react";
 import Pattern from "../../../assets/welcomePattern.svg";
+import LoginButton from "../../auth/LoginButton";
 import * as S from "./Welcome.styles";
+import { onAuthStateChanged, User } from "firebase/auth";
+import { auth } from "../../auth/utils/firebaseConfig";
 
 const Welcome = () => {
+  const [user, setUser] = useState<User | null>(null);
+
+  // 로그인 상태 추적
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser);
+      } else {
+        setUser(null);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
     <S.WelcomeContainer>
       <S.Pattern src={Pattern} />
       <S.ContentContainer>
         <S.Title>Welcome to Maily :)</S.Title>
         <S.WelcomePhrase>당신의 매일을 기록하세요.</S.WelcomePhrase>
-        <S.LoginPhrase>로그인 후 Maily를 이용해 보세요! ↓</S.LoginPhrase>
-        <LoginButton />
+        {user ? (
+          // 로그인 됐을 때
+          <></>
+        ) : (
+          <>
+            {/* 로그인 안 됐을 때 */}
+            <S.LoginPhrase>로그인 후 Maily를 이용해 보세요! ↓</S.LoginPhrase>
+            <LoginButton />
+          </>
+        )}
       </S.ContentContainer>
     </S.WelcomeContainer>
   );
