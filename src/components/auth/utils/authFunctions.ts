b588a7d54f89332519ac6f6467f8d8d256029ab3
h-0y28 <s24015@gsm.hs.kr>
2024-12-26 19:59:29 +0900

    feat: authFunctions

--- src/components/auth/utils/authFunctions.ts
+++ src/components/auth/utils/authFunctions.ts
@@ -1,14 +1,41 @@
-import { signInWithPopup } from "firebase/auth";
-import { auth, provider } from "./firebase";
+import { useState, useEffect } from "react";
+import { auth, provider } from "./firebaseConfig";
+import { signInWithPopup, onAuthStateChanged, User } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 
-export const handleLogin = async (navigate: ReturnType<typeof useNavigate>) => {
-  try {
-    const result = await signInWithPopup(auth, provider);
-    const user = result.user;
-    console.log("User logged in: ", user);
+const useAuth = () => {
+  const [user, setUser] = useState<User | null>(null);
+  const navigate = useNavigate();
+
+  // 로그인 함수
+  const login = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      setUser(result.user);
+    } catch (error) {
+      console.error("Login error:", error);
+    }
     navigate("/home");
-  } catch (error) {
-    console.error("Login error: ", error);
-  }
+  };
+
+  // 로그아웃 함수
+  const logout = async () => {
+    try {
+      await auth.signOut();
+      setUser(null);
+    } catch (error) {
+      console.error("Logout error:", error);
+    }
+    navigate("/"); // intro page
+  };
+
+  // 유저 상태 추적
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, setUser);
+    return () => unsubscribe();
+  }, []);
+
+  return { user, login, logout };
 };
+
+export default useAuth;
