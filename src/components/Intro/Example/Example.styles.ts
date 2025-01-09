490440f13854f00f3586af72bb64f04e7c2f3914
h-0y28 <s24015@gsm.hs.kr>
2025-01-09 20:52:52 +0900

    style: 디자인 반영 - diary example

--- src/components/Intro/Example/Example.styles.ts
+++ src/components/Intro/Example/Example.styles.ts
@@ -4,15 +4,26 @@ import { theme } from "../../../styles/theme";
 export const ExampleContainer = styled.div`
   display: flex;
   color: ${theme.colors.primaryBlack};
-  height: 50vh;
-
-  & > * {
-    flex: 1;
-  }
+  height: fit-content;
 `;
 
 export const Pattern = styled.div`
   background-color: ${theme.colors.primaryYellow};
-  width: 50%;
-  height: 100%;
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const DiaryWrapper = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  padding: 0;
+
+  & > div {
+    padding: 2.5rem 2.7rem 0 3.5rem;
+    box-sizing: border-box;
+  }
 `;
