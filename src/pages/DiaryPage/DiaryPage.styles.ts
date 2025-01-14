da0824a25cda58b7e4abd798e23d1f0185bf74b3
h-0y28 <s24015@gsm.hs.kr>
2025-01-14 20:56:27 +0900

    style: 크기 조정정

--- src/pages/DiaryPage/DiaryPage.styles.ts
+++ src/pages/DiaryPage/DiaryPage.styles.ts
@@ -3,7 +3,7 @@ import { theme } from "../../styles/theme";
 
 export const DiaryPageContainer = styled.div`
   display: grid;
-  grid-template-columns: 3fr 2fr;
+  grid-template-columns: 2fr 2fr;
   gap: 9rem;
 `;
 
@@ -13,7 +13,7 @@ export const EmptyContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 1rem;
-  align-items: start;
+  align-items: center;
   justify-content: center;
 
   padding: 8rem;
@@ -23,7 +23,7 @@ export const EmptyContainer = styled.div`
   border: 2px solid ${theme.colors.primaryBrown};
   border-radius: 2rem;
 
-  width: fit-content;
+  width: 100%;
   height: fit-content;
 
   margin: auto;
