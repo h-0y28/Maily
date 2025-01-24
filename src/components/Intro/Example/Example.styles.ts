c427b5ad34a029659d18aa8b091d456f61fc9b47
h-0y28 <s24015@gsm.hs.kr>
2025-01-24 16:11:56 +0900

    change: component -> img

--- src/components/Intro/Example/Example.styles.ts
+++ src/components/Intro/Example/Example.styles.ts
@@ -1,18 +1,9 @@
 import styled from "styled-components";
 import { theme } from "../../../styles/theme";
 
-export const ExampleContainer = styled.div`
-  display: flex;
-  color: ${theme.colors.primaryBlack};
-  height: fit-content;
-`;
-
 export const Pattern = styled.div`
   background-color: ${theme.colors.primaryYellow};
   flex: 1;
-  display: flex;
-  align-items: center;
-  justify-content: center;
 `;
 
 export const DiaryWrapper = styled.div`
@@ -22,8 +13,23 @@ export const DiaryWrapper = styled.div`
   justify-content: center;
   padding: 0;
 
-  & > div {
+  /* & > div {
     padding: 2.5rem 2.7rem 0 3.5rem;
     box-sizing: border-box;
-  }
+  } */
+`;
+
+export const ExampleImg = styled.img`
+  max-width: 100%;
+  max-height: 100%;
+  object-fit: contain;
+  padding: 2.5rem 2.7rem 0 3.5rem;
+`;
+
+export const ExampleContainer = styled.div`
+  display: flex;
+  color: ${theme.colors.primaryBlack};
+  height: 100vh;
+  width: 100%;
+  overflow: hidden;
 `;
--- src/components/Intro/Example/Example.tsx
+++ src/components/Intro/Example/Example.tsx
@@ -1,15 +1,26 @@
-import CalendarComponent from "../../Calendar";
-import DiaryComponent from "../../Diary/DiaryComponent";
+// import CalendarComponent from "../../Calendar";
+// import DiaryComponent from "../../Diary/DiaryComponent";
+
+import ExampleDiary from "../../../assets/exampleDiary.png";
+import ExampleCalender from "../../../assets/exampleCalender.png";
+
 import * as S from "./Example.styles";
 
 const Example = () => {
   return (
     <S.ExampleContainer>
-      <S.Pattern>
+      {/* <S.Pattern>
         <CalendarComponent />
       </S.Pattern>
       <S.DiaryWrapper>
         <DiaryComponent />
+      </S.DiaryWrapper> */}
+
+      <S.Pattern>
+        <S.ExampleImg src={ExampleCalender}></S.ExampleImg>
+      </S.Pattern>
+      <S.DiaryWrapper>
+        <S.ExampleImg src={ExampleDiary}></S.ExampleImg>
       </S.DiaryWrapper>
     </S.ExampleContainer>
   );
