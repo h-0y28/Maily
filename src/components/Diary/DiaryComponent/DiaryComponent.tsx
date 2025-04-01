453ac941d0e462caab199b41044827c3613d1d0c
h-0y28 <s24015@gsm.hs.kr>
2025-04-02 00:12:16 +0900

    chore: 필요없는 코드 삭제 ( example diary )

--- src/components/Diary/DiaryComponent/DiaryComponent.tsx
+++ src/components/Diary/DiaryComponent/DiaryComponent.tsx
@@ -20,45 +20,14 @@ export default function DiaryComponent() {
   const navigate = useNavigate();
   const location = useLocation();
   const { user } = useAuth();
-  const date = location.pathname.split("/diary/")[1] || "example";
+  const date = location.pathname.split("/diary/")[1];
 
   const [diary, setDiary] = useState<Diary | null>(null);
   const [loading, setLoading] = useState(true);
 
-  const exampleDiary: Diary = {
-    id: "example",
-    date: "2024.08.02",
-    title: "내 생일, 나를 위한 하루",
-    content: `
-오늘은 내 생일이다. 아침에 눈을 뜨자마자 왠지 모르게 설레는 기분이 들었다. 평소와 다르지 않은 하루처럼 보였지만, 오늘만큼은 나를 위한 날이라는 생각에 마음이 따뜻해졌다.
-
-[하루 시작]
-아침에 가족들이 준비한 작은 이벤트가 있었다. 엄마가 내게 좋아하는 음식으로 아침을 차려주셨다. 작은 일이지만 마음이 꽉 차는 기분이었다.
-
-[축하 메시지와 선물]
-오늘 하루 종일 친구들에게서 축하 메시지를 받았다. 평소에 연락이 뜸했던 친구까지 메시지를 보내줘서 정말 고마웠다. OO가 보내준 편지는 읽고 정말 감동했다.
-
-[특별한 순간]
-오늘은 가족과 함께 맛있는 케이크를 먹으며 사진도 찍었다. 케이크를 자르는 순간, 정말 행복했다.
-내가 태어난 날을 이렇게 축하받을 수 있다는 게 참 감사하게 느껴졌다.
-
-[다짐과 목표]
-올해는 더 성장하고, 나 자신을 더 사랑하는 한 해가 되었으면 좋겠다. 내년 생일에는 지금의 나보다 더 멋지고, 행복한 내가 되어 있기를 바라본다.
-
-[마무리]
-오늘 하루는 정말 완벽했다. 평범한 일상 속에서 특별한 순간을 느낄 수 있다는 게 얼마나 감사한 일인지 깨달았다. 내 곁에 있어준 모든 사람들에게 고맙다는 말을 전하고 싶다. 그리고 오늘의 나는, 정말 행복했다.
-`,
-  };
-
   const fetchDiary = async () => {
     if (!user || !date) {
       console.error("User or diaryDate is missing!");
-      return;
-    }
-
-    if (date === "example") {
-      setDiary(exampleDiary);
-      setLoading(false);
       return;
     }
 
@@ -94,7 +63,7 @@ export default function DiaryComponent() {
   }, [user, date]);
 
   const deleteDiary = async () => {
-    if (!diary || diary.id === "example") return;
+    if (!diary) return;
 
     try {
       const diaryRef = doc(db, "diaries", diary.id);
