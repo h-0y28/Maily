3d4bd7a5749361c3db99d8900b517a3485a3a282
h-0y28 <s24015@gsm.hs.kr>
2025-04-16 21:15:24 +0900

    style: 반응형 추가

--- src/layout/Header/Header.styles.ts
+++ src/layout/Header/Header.styles.ts
@@ -8,6 +8,28 @@ export const HeaderWrapper = styled.header`
   margin: 0rem 15rem;
   padding: 0.5rem 1rem;
   border-bottom: 1px solid ${theme.colors.primaryBrown};
+
+  @media screen and (max-width: 1728px) {
+    margin: 0rem 13rem;
+  }
+  @media screen and (max-width: 1600px) {
+    margin: 0rem 11rem;
+  }
+  @media screen and (max-width: 1440px) {
+    margin: 0rem 9rem;
+  }
+  @media screen and (max-width: 1200px) {
+    margin: 0rem 6rem;
+  }
+  @media screen and (max-width: 1024px) {
+    margin: 0rem 3rem;
+  }
+  @media screen and (max-width: 768px) {
+    margin: 0rem 1rem;
+  }
+  @media screen and (max-width: 480px) {
+    margin: 0rem 1rem;
+  }
 `;
 
 export const Logo = styled.img`
