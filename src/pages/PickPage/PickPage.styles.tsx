cd7d29cef0aba4b8e8a23a36430c2b9ee399c7a0
h-0y28 <s24015@gsm.hs.kr>
2025-05-07 15:19:04 +0900

    style: 스타일 수정

--- src/pages/PickPage/PickPage.styles.tsx
+++ src/pages/PickPage/PickPage.styles.tsx
@@ -1,13 +1,35 @@
 import styled from "styled-components";
 import { theme } from "../../styles/theme";
 
-export const PcikPageContainer = styled.div``;
+export const Header = styled.div`
+  display: flex;
 
-export const PickPageTitle = styled.h1`
-  font-size: 2rem;
+  background-color: ${theme.colors.primaryBackground};
+  padding: 3rem 6.25rem;
+`;
+
+export const Title = styled.h1`
+  font-size: 2.5rem;
   font-weight: 700;
+  color: ${theme.colors.primaryBrown};
+`;
+
+export const InfoBox = styled.div`
+  display: flex;
+  align-items: center;
+
+  margin-left: 2.5rem;
+  font-size: 1rem;
+`;
+
+export const Description = styled.p`
   color: ${theme.colors.primaryBlack};
-  margin-bottom: 2rem;
+`;
+
+export const EditButton = styled.button`
+  color: ${theme.colors.primaryRed};
+  background-color: transparent;
+  border: none;
 `;
 
 export const CardContainer = styled.div`
