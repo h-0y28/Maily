89da59eae6548392b4c40c8b0d206a68d2d6a4f9
h-0y28 <s24015@gsm.hs.kr>
2025-05-28 15:29:46 +0900

    refactor: 필요없는 컴포넌트 삭제

--- src/components/auth/LoginButton/LoginButton.tsx
+++
@@ -1,9 +0,0 @@
-import useAuth from "../utils/authFunctions";
-import * as S from "./LoginButton.styles";
-
-const LoginButton = () => {
-  const { login } = useAuth();
-  return <S.ButtonContainer onClick={login}>Google로 로그인</S.ButtonContainer>;
-};
-
-export default LoginButton;
