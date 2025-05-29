3406313feac8519864e155e0fde14d1547d431a6
h-0y28 <s24015@gsm.hs.kr>
2025-05-29 09:39:40 +0900

    style: 반응형 추가

--- src/styles/commonStyles.ts
+++ src/styles/commonStyles.ts
@@ -1,9 +1,24 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  width: 100%;
   padding-left: 15rem;
   padding-right: 15rem;
   padding-bottom: 9rem;
 
-  width: 100%;
+  @media (max-width: 1024px) {
+    padding-left: 8rem;
+    padding-right: 8rem;
+    padding-bottom: 7rem;
+  }
+  @media (max-width: 768px) {
+    padding-left: 4rem;
+    padding-right: 4rem;
+    padding-bottom: 5rem;
+  }
+  @media (max-width: 480px) {
+    padding-left: 1.5rem;
+    padding-right: 1.5rem;
+    padding-bottom: 3rem;
+  }
 `;
