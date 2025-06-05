c2e846245bf4ce818acb5d5023c6633a6ebe5642
h-0y28 <s24015@gsm.hs.kr>
2025-06-05 09:18:51 +0900

    style: Pretendard 폰트 URL 및 가중치 수정

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -3,8 +3,8 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
   @font-face {
     font-family: "Pretendard";
-    src: url("https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2111@1.0/Pretendard-Regular.woff2") format("woff2");
-    font-weight: normal;
+    src: url("https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.0/Pretendard-Regular.woff2") format("woff2");
+    font-weight: 400;
     font-style: normal;
   }
 
