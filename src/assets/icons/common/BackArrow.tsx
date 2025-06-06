72bca117c062971a545dd414bfe38d369642f544
h-0y28 <s24015@gsm.hs.kr>
2025-06-06 23:30:38 +0900

    feat: BackArrow 및 CloseButton 아이콘 추가

---
+++ src/assets/icons/common/BackArrow.tsx
@@ -0,0 +1,19 @@
+const BackArrow = ({ width = "3rem", height = "3rem" }) => {
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox="0 0 48 49"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M22.7751 1.4873L2.77515 23.9899M22.7751 47.2977L2.77515 23.9899M2.77515 23.9899H47.8516"
+        stroke="#A67B5B"
+        stroke-width="3"
+      />
+    </svg>
+  );
+};
+
+export default BackArrow;
