3f53580c0f69dfc0ea63617f119a64b1a1c4a3df
h-0y28 <s24015@gsm.hs.kr>
2025-06-08 23:59:30 +0900

    chore: 주석 처리된 아이콘 임포트 및 내보내기 정리

--- src/assets/icons/diary/index.ts
+++ src/assets/icons/diary/index.ts
@@ -1,49 +1,53 @@
-import FillHeart from "./FillHeartIcon.png";
-import EmptyHeartIcon from "./EmptyHeartIcon.png";
+// import FillHeart from "./FillHeartIcon.png";
+// import EmptyHeartIcon from "./EmptyHeartIcon.png";
 
-// 날씨 아이콘
-import WeathersIcon from "./Weathers.png";
-import SunnyIcon from "./SunnyIcon.png";
-import CloudyInTheSunIcon from "./CloudyInTheSunIcon.png";
-import CloudyIcon from "./CloudyIcon.png";
-import RainyIcon from "./RainyIcon.png";
-import WindyIcon from "./WindyIcon.png";
-import SnowyIcon from "./SnowyIcon.png";
+// // 날씨 아이콘
+// import WeathersIcon from "./Weathers.png";
+// import SunnyIcon from "./SunnyIcon.png";
+// import CloudyInTheSunIcon from "./CloudyInTheSunIcon.png";
+// import CloudyIcon from "./CloudyIcon.png";
+// import RainyIcon from "./RainyIcon.png";
+// import WindyIcon from "./WindyIcon.png";
+// import SnowyIcon from "./SnowyIcon.png";
 
-// 기분 아이콘
-import FeelingsIcon from "./Feelings.png";
-import GoodIcon from "./GoodIcon.png";
-import HappyIcon from "./HappyIcon.png";
-import NiceIcon from "./NiceIcon.png";
-import LoveIcon from "./LoveIcon.png";
-import SadIcon from "./SadIcon.png";
-import BadIcon from "./BadIcon.png";
-import AngryIcon from "./AngryIcon.png";
-import SickIcon from "./SickIcon.png";
-import SleepingIcon from "./SleepingIcon.png";
-import MoneyIcon from "./MoneyIcon.png";
-import HotIcon from "./HotIcon.png";
+// // 기분 아이콘
+// import FeelingsIcon from "./Feelings.png";
+// import GoodIcon from "./GoodIcon.png";
+// import HappyIcon from "./HappyIcon.png";
+// import NiceIcon from "./NiceIcon.png";
+// import LoveIcon from "./LoveIcon.png";
+// import SadIcon from "./SadIcon.png";
+// import BadIcon from "./BadIcon.png";
+// import AngryIcon from "./AngryIcon.png";
+// import SickIcon from "./SickIcon.png";
+// import SleepingIcon from "./SleepingIcon.png";
+// import MoneyIcon from "./MoneyIcon.png";
+// import HotIcon from "./HotIcon.png";
 
-export {
-  FillHeart,
-  EmptyHeartIcon,
-  WeathersIcon,
-  SunnyIcon,
-  CloudyInTheSunIcon,
-  CloudyIcon,
-  RainyIcon,
-  WindyIcon,
-  SnowyIcon,
-  FeelingsIcon,
-  GoodIcon,
-  HappyIcon,
-  NiceIcon,
-  LoveIcon,
-  SadIcon,
-  BadIcon,
-  AngryIcon,
-  SickIcon,
-  SleepingIcon,
-  MoneyIcon,
-  HotIcon,
-};
+// export {
+//   FillHeart,
+//   EmptyHeartIcon,
+//   WeathersIcon,
+//   SunnyIcon,
+//   CloudyInTheSunIcon,
+//   CloudyIcon,
+//   RainyIcon,
+//   WindyIcon,
+//   SnowyIcon,
+//   FeelingsIcon,
+//   GoodIcon,
+//   HappyIcon,
+//   NiceIcon,
+//   LoveIcon,
+//   SadIcon,
+//   BadIcon,
+//   AngryIcon,
+//   SickIcon,
+//   SleepingIcon,
+//   MoneyIcon,
+//   HotIcon,
+// };
+
+export { default as SleepingIcon } from "./SleepingIcon";
+export { default as MoneyIcon } from "./MoneyIcon";
+export { default as HotIcon } from "./HotIcon";
