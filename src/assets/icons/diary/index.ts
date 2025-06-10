996dbff55ce3e788e9dda7e33ccfbac86ab0f4d4
h-0y28 <s24015@gsm.hs.kr>
2025-06-10 15:09:14 +0900

    chore: 주석 처리된 아이콘 임포트 제거 및 코드 정리

--- src/assets/icons/diary/index.ts
+++ src/assets/icons/diary/index.ts
@@ -1,53 +1,21 @@
-// import FillHeart from "./FillHeartIcon.png";
-// import EmptyHeartIcon from "./EmptyHeartIcon.png";
-
-// // 날씨 아이콘
-// import WeathersIcon from "./Weathers.png";
-// import SunnyIcon from "./SunnyIcon.png";
-// import CloudyInTheSunIcon from "./CloudyInTheSunIcon.png";
-// import CloudyIcon from "./CloudyIcon.png";
-// import RainyIcon from "./RainyIcon.png";
-// import WindyIcon from "./WindyIcon.png";
-// import SnowyIcon from "./SnowyIcon.png";
-
-// // 기분 아이콘
-// import FeelingsIcon from "./Feelings.png";
-// import GoodIcon from "./GoodIcon.png";
-// import HappyIcon from "./HappyIcon.png";
-// import NiceIcon from "./NiceIcon.png";
-// import LoveIcon from "./LoveIcon.png";
-// import SadIcon from "./SadIcon.png";
-// import BadIcon from "./BadIcon.png";
-// import AngryIcon from "./AngryIcon.png";
-// import SickIcon from "./SickIcon.png";
-// import SleepingIcon from "./SleepingIcon.png";
-// import MoneyIcon from "./MoneyIcon.png";
-// import HotIcon from "./HotIcon.png";
-
-// export {
-//   FillHeart,
-//   EmptyHeartIcon,
-//   WeathersIcon,
-//   SunnyIcon,
-//   CloudyInTheSunIcon,
-//   CloudyIcon,
-//   RainyIcon,
-//   WindyIcon,
-//   SnowyIcon,
-//   FeelingsIcon,
-//   GoodIcon,
-//   HappyIcon,
-//   NiceIcon,
-//   LoveIcon,
-//   SadIcon,
-//   BadIcon,
-//   AngryIcon,
-//   SickIcon,
-//   SleepingIcon,
-//   MoneyIcon,
-//   HotIcon,
-// };
-
+export { default as FillHeart } from "./FillHeart";
+export { default as EmptyHeartIcon } from "./EmptyHeartIcon";
+export { default as WeathersIcon } from "./WeathersIcon";
+export { default as SunnyIcon } from "./SunnyIcon";
+export { default as CloudyInTheSunIcon } from "./CloudyInTheSunIcon";
+export { default as CloudyIcon } from "./CloudyIcon";
+export { default as RainyIcon } from "./RainyIcon";
+export { default as WindyIcon } from "./WindyIcon";
+export { default as SnowyIcon } from "./SnowyIcon";
+export { default as FeelingsIcon } from "./FeelingsIcon";
+export { default as GoodIcon } from "./GoodIcon";
+export { default as HappyIcon } from "./HappyIcon";
+export { default as NiceIcon } from "./NiceIcon";
+export { default as LoveIcon } from "./LoveIcon";
+export { default as SadIcon } from "./SadIcon";
+export { default as BadIcon } from "./BadIcon";
+export { default as AngryIcon } from "./AngryIcon";
+export { default as SickIcon } from "./SickIcon";
 export { default as SleepingIcon } from "./SleepingIcon";
 export { default as MoneyIcon } from "./MoneyIcon";
 export { default as HotIcon } from "./HotIcon";
