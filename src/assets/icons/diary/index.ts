8d587f27fc2f45c694d391735420e0ff8f940a94
h-0y28 <s24015@gsm.hs.kr>
2025-06-11 23:40:37 +0900

    Add AngryIcon to diary icon exports

--- src/assets/icons/diary/index.ts
+++ src/assets/icons/diary/index.ts
@@ -1,6 +1,8 @@
 export { default as FillHeart } from "./FillHeart";
 export { default as EmptyHeartIcon } from "./EmptyHeartIcon";
 export { default as WeathersIcon } from "./WeathersIcon";
+
+export { default as AngryIcon } from "./AngryIcon";
 export { default as SunnyIcon } from "./SunnyIcon";
 export { default as CloudyInTheSunIcon } from "./CloudyInTheSunIcon";
 export { default as CloudyIcon } from "./CloudyIcon";
@@ -14,7 +16,6 @@ export { default as NiceIcon } from "./NiceIcon";
 export { default as LoveIcon } from "./LoveIcon";
 export { default as SadIcon } from "./SadIcon";
 export { default as BadIcon } from "./BadIcon";
-export { default as AngryIcon } from "./AngryIcon";
 export { default as SickIcon } from "./SickIcon";
 export { default as SleepingIcon } from "./SleepingIcon";
 export { default as MoneyIcon } from "./MoneyIcon";
