59bd9af10bfda4d85d3f37b72f50c68ebcef1012
h-0y28 <s24015@gsm.hs.kr>
2025-06-14 22:52:09 +0900

    chore: 아이콘 내보내기 순서 정리

--- src/assets/icons/diary/index.ts
+++ src/assets/icons/diary/index.ts
@@ -1,15 +1,16 @@
 export { default as FillHeart } from "./FillHeart";
 export { default as EmptyHeartIcon } from "./EmptyHeartIcon";
-export { default as WeathersIcon } from "./WeathersIcon";
 
-export { default as AngryIcon } from "./AngryIcon";
+export { default as WeathersIcon } from "./WeathersIcon";
 export { default as SunnyIcon } from "./SunnyIcon";
 export { default as CloudyInTheSunIcon } from "./CloudyInTheSunIcon";
 export { default as CloudyIcon } from "./CloudyIcon";
 export { default as RainyIcon } from "./RainyIcon";
 export { default as WindyIcon } from "./WindyIcon";
 export { default as SnowyIcon } from "./SnowyIcon";
+
 export { default as FeelingsIcon } from "./FeelingsIcon";
+export { default as AngryIcon } from "./AngryIcon";
 export { default as GoodIcon } from "./GoodIcon";
 export { default as HappyIcon } from "./HappyIcon";
 export { default as NiceIcon } from "./NiceIcon";
